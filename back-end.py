ece18d90c572f89f85dda202eaf5bb1260406316
Victor <el.rachman@unesp.br>
2026-04-17 09:12:34 -0300

    sistema de login com bd

--- back-end.py
+++ back-end.py
@@ -1,24 +1,74 @@
+#importa bibliotecas
+import sqlite3
 from flask import Flask, request, redirect, session
 from flask_cors import CORS
 
+#iniciando o flask
 app = Flask(__name__)
 app.secret_key = "123"
 CORS(app, supports_credentials=True, origins=["http://127.0.0.1:5500"])
 
+#iniciando banco de dados
+def init_db():
+    conn = sqlite3.connect('pystudy.db')
+    cur = conn.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT,
+            senha TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+init_db()
+
+#verifica se esta logado
 @app.route('/status')
 def status():
     return {"logado": session.get("logado", False)}
 
+#faz login
 @app.route('/login', methods=['POST'])
 def recebe_dados():
     nome = request.form.get('nome')
-    session['logado'] = True
+    senha = request.form.get('senha')
 
-    return redirect('http://127.0.0.1:5500/graficos.html')
+    conn = sqlite3.connect('pystudy.db')
+    cur = conn.cursor()
 
-@app.route('cadastro', methods=['POST'])
+    cur.execute("SELECT * FROM usuarios WHERE nome=? AND senha=?", (nome, senha))
+    user = cur.fetchone()
+    conn.close()
+
+    if user:
+        session['logado'] = True
+        return redirect('http://127.0.0.1:5500/graficos.html')
+    else:
+        session['logado'] = False
+        return redirect('http://127.0.0.1:5500/index.html')
+
+
+   
+
+#faz cadastro
+@app.route('/cadastro', methods=['POST'])
 def cadastrar():
-    session['logado'] = True
-    return redirect('http://127.0.0.1:5500/graficos.html')
+    nome = request.form.get('nome')
+    senha = request.form.get('senha')
 
+    conn = sqlite3.connect('pystudy.db')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO usuarios (nome, senha) VALUES (?, ?)", (nome, senha))
+
+    conn.commit()
+    conn.close()
+
+    return redirect('http://127.0.0.1:5500/index.html')
+
+#roda o flask
 app.run(debug=True)
