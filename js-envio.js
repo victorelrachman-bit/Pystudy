67832a42448fb5904cbed3531e8340822a36b1d9
Victor <el.rachman@unesp.br>
2026-05-08 08:47:14 -0300

    pystudy novos comecos

--- js-envio.js
+++ js-envio.js
@@ -1,9 +1,15 @@
 async function verificar() {
-    const log = await fetch("http://127.0.0.1:5000/status", {
-        credentials: "include"
-    });
-    const log_json = await log.json()
-    if(!log_json.logado)
+    try{
+        const log = await fetch("http://127.0.0.1:5000/status", {
+            credentials: "include"
+        });
+
+        const log_json = await log.json()
+
+        if(!log_json.logado) {
+            window.location.href = "http://127.0.0.1:5500/index.html"
+        }
+    }catch(error)
     {
         window.location.href = "http://127.0.0.1:5500/index.html"
     }
--- js-graficos.js
+++ js-graficos.js
@@ -1,11 +1,16 @@
 async function verificar() {
-    const log = await fetch("http://127.0.0.1:5000/status", {
-        credentials: "include"
-    });
+    try{
+        const log = await fetch("http://127.0.0.1:5000/status", {
+            credentials: "include"
+        });
 
-    const log_json = await log.json()
+        const log_json = await log.json()
 
-    if(!log_json.logado) {
+        if(!log_json.logado) {
+            window.location.href = "http://127.0.0.1:5500/index.html"
+        }
+    }catch(error)
+    {
         window.location.href = "http://127.0.0.1:5500/index.html"
     }
 }
